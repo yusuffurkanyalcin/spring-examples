024f15fdab0f64db56ac8654ad502d23fa6278ea
Yusuf Furkan Yalçın <55233993+yusuffurkanyalcin@users.noreply.github.com>
2022-02-21 22:14:49 +0300

    Delete spring-examples directory

--- spring-examples/spring-mongo-rest-api/src/main/java/com/example/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import com.example.entities.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String> {
-
-
-}
