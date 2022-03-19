fe4b2b3d3dc11e115e94aa1884e56e59e966333b
Yusuf Furkan Yalçın <55233993+yusuffurkanyalcin@users.noreply.github.com>
2022-03-19 12:07:39 +0300

    Add files via upload

---
+++ fake-service/src/main/java/server/entity/User.java
@@ -0,0 +1,40 @@
+package server.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public User(int id, String firstName, String lastName){
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+
+}
