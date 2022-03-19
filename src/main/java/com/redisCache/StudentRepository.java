a716ed352af6e4c73dc7363feffcba75d2c75170
Yusuf Furkan Yalçın <55233993+yusuffurkanyalcin@users.noreply.github.com>
2022-03-19 12:13:49 +0300

    Delete src/main/java/com/redisCache directory

--- src/main/java/com/redisCache/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.redisCache;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student,String> {
-}
